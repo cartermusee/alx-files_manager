f90ff4fbbb2130fd9c5acf0af1c65d1c0ddbc9e3
macilah-carter <museemacilah@gmail.com>
2024-03-13 14:50:37 +0300

    debugging postupload

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -16,34 +16,34 @@ async function getUserIdByToken(token) {
 class FilesController {
   static async postUpload(req, res) {
     const token = req.headers['x-token'];
-
+  
     if (!token) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
-
+  
     const {
       name, type, parentId = 0, isPublic = false, data,
     } = req.body;
-
+  
     if (!name) {
       return res.status(400).json({ error: 'Missing name' });
     }
-
+  
     if (!type || !['folder', 'file', 'image'].includes(type)) {
       return res.status(400).json({ error: 'Missing or invalid type' });
     }
-
+  
     if ((type !== 'folder' && !data) || (type === 'folder' && data)) {
-      return res.status(400).json({ error: 'Invalid data for the given type' });
+      return res.status(400).json({ error: 'Missing data' });
     }
-
+  
     try {
       const userId = await getUserIdByToken(token);
-
+  
       if (!userId) {
         return res.status(401).json({ error: 'Unauthorized' });
       }
-
+  
       if (parentId !== 0) {
         const parentFile = await dbClient.getFileById(parentId);
         if (!parentFile) {
@@ -53,23 +53,23 @@ class FilesController {
           return res.status(400).json({ error: 'Parent is not a folder' });
         }
       }
-
+  
       let localPath = '';
-
+  
       if (type !== 'folder') {
         const folderPath = process.env.FOLDER_PATH || '/tmp/files_manager';
         if (!fs.existsSync(folderPath)) {
           fs.mkdirSync(folderPath, { recursive: true });
         }
-
+  
         const fileUUID = uuidv4();
         localPath = `${folderPath}/${fileUUID}`;
-
+  
         // Save the file content to local path
         const fileBuffer = Buffer.from(data, 'base64');
         fs.writeFileSync(localPath, fileBuffer);
       }
-
+  
       const newFile = await dbClient.createFile(userId, name, type, parentId, isPublic, localPath);
       const response = {
         id: newFile._id,
@@ -84,7 +84,7 @@ class FilesController {
       console.error(error);
       return res.status(500).json({ error: 'Internal Server Error' });
     }
-  }
+  }  
 
   static async getShow(req, res) {
     const token = req.headers['x-token'];
